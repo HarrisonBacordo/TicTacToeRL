0f22e25e15ef85aa1dd7a3df1ac5760ad1aeab4d
Harrison Bacordo <bacordharr@myvuw.ac.nz>
2018-08-08 22:59:36 +1200

    Progress on training loop

--- src/network.py
+++ src/network.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from src.Board import Board
 
 # training parameters
 num_epochs = 20000
@@ -16,8 +17,7 @@ decay_rate = 0.1
 # setup
 global_step = tf.Variable(0, trainable=False)
 init = tf.global_variables_initializer()
-
-
+saver = tf.train.Saver()
 
 # placeholders
 x = tf.placeholder(shape=[None, num_inputs], dtype=tf.float32)
@@ -47,6 +47,11 @@ outV = tf.add(tf.matmul(in_h, weights['h_oV']), biases['h_oV'])
 # general functions for both loss evaluations
 learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, decay_steps, decay_rate)
 
+# Action metrics to return
+probs = tf.nn.softmax(outA)
+max_action = tf.argmax(probs)
+max_prob = tf.reduce_max(probs)
+
 # loss for action head
 lossA = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=yA, logits=outA))
 optimizerA = tf.train.GradientDescentOptimizer(learning_rate).minimize(lossA, global_step=global_step)
@@ -56,13 +61,22 @@ lossV = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=yV, log
 optimizerV = tf.train.GradientDescentOptimizer(learning_rate).minimize(lossA, global_step=global_step)
 
 with tf.Session() as sess:
+    board = Board()
+    probabilities = []
     states = []
     actions = []
     rewards = []
     values = []
     sess.run(init)
     for epoch in range(num_epochs):
-        for i in range(batch_size):
-            _, cA, _, cV = sess.run([optimizerA, lossA, optimizerV, lossV], feed_dict={x: states[i],
-                                                                                       yA: actions[i],
-                                                                                       yV: values[i]})
+        saver.save(sess, save_path='../saved_models.ckpt')
+        current_state = board.start()
+        states.append(current_state)
+        while board.winner(states) == 0:
+            action, action_prob, prob = sess.run([max_action, max_prob, probs],
+                                                 feed_dict={x: current_state})
+            current_state = board.next_state(current_state, action)
+            states.append(current_state)
+            actions.append(action)
+            probabilities.append(prob)
+#       TODO LOGIC FOR BACKPROP HERE
